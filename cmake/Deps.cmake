192de9e4941fd54109df04516f9b096295312aa8
RoBaertschi <rtmbaertschi007@gmail.com>
2023-11-13 10:53:41 +0100

    Some Work Done

--- cmake/Deps.cmake
+++ cmake/Deps.cmake
@@ -1,13 +1,31 @@
 
 include(FetchContent)
-FetchContent_Declare(
-        ptw
-        GIT_REPOSITORY https://github.com/GerHobbelt/pthread-win32
-        GIT_TAG version-3.1.0-release
-)
-FetchContent_GetProperties(ptw ptw_SOURCE_DIR)
-message("${ptw_SOURCE_DIR}")
-set(CMAKE_MODULE_PATH "${CMAKE_MODULE_PATH}" "${ptw_SOURCE_DIR}/cmake")
-message(${CMAKE_MODULE_PATH})
+if (${WIN32})
+    add_subdirectory(libs/pthread-win32)
+endif ()
 
-FetchContent_MakeAvailable(ptw)
+# TODO: Find out how to set stuff for FetchContent projects
+
+if (${GLFW_SUPPORT})
+    FetchContent_Declare(
+            glfw
+            GIT_REPOSITORY https://github.com/glfw/glfw/
+            GIT_TAG 3.3.8
+    )
+    set(GLFW_BUILD_DOCS OFF)
+    set(GLFW_BUILD_EXAMPLES OFF)
+    set(GLFW_BUILD_TESTS OFF)
+    set(GLFW_INSTALL OFF)
+    FetchContent_MakeAvailable(glfw)
+endif ()
+
+if (${SDL2_SUPPORT})
+    FetchContent_Declare(
+            sdl2
+            GIT_REPOSITORY https://github.com/libsdl-org/SDL/
+            GIT_TAG release-2.28.5
+    )
+    set(SDL2_DISABLE_SDL2MAIN ON CACHE BOOL "Disables SDL2's main" FORCE)
+    set(SDL2_DISABLE_INSTALL ON CACHE BOOL "Disables SDL2's install" FORCE)
+    FetchContent_MakeAvailable(sdl2)
+endif ()
